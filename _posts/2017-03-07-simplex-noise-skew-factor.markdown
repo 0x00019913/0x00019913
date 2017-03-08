8a7333a0e41473ae7213e1e751eef4db57d2b83f
Anton Bovin <0x00019913@gmail.com>
2017-03-08 00:28:21 -0600

    header works

--- _posts/2017-03-07-simplex-noise-skew-factor.markdown
+++ _posts/2017-03-07-simplex-noise-skew-factor.markdown
@@ -7,7 +7,7 @@ excerpt: "I explain the simplex noise skew factor."
 date: 2017-03-07 16:00:00
 ---
 
-So I was reading about simplex noise <a href="http://weber.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf">in the Gustavson paper</a>, right? If one understands Perlin ("classic") noise, everything's pretty straighforward by analogy, at least in 2D: imagine that your plane is partitioned into a lattice of triangles (of some size and orientation); then hash the coordinates of every triangle corner to produce a pseudo-random gradient. Now, it would be nice if the vertices were located at integer coordinates instead - so we perform whatever magic is needed to get each triangle vertex to align with a pair of integer coordinates.
+So I was reading about simplex noise <a href="http://weber.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf">in the Gustavson paper</a>, right? If one understands Perlin ("classic") noise, everything's pretty straighforward by analogy, at least in 2D: imagine that your plane is partitioned into a lattice of triangles, then hash the coordinates of every triangle corner to produce a pseudo-random gradient. Now, it would be nice if the vertices were located at integer coordinates instead - so we perform whatever magic is needed to get each triangle vertex to align with a pair of integer coordinates.
 
 > Did you know? Simplex noise was invented in 1836 by Edwin Simplex of Lancastershire, who was never terribly fond of his name.
 
@@ -34,7 +34,7 @@ double X0 = i-t;
 double Y0 = j-t;
 ```
 
-So we just stretch the triangle grid along the $$x=y$$ line, a linear transformation. But what are these factors of $$\frac{\sqrt{3}-1}{2}$$ and $$\frac{\sqrt{3}-3}{6}$$? I worked it out for myself from first principles with some reverse-engineering.
+So we just stretch the triangle grid along the $$x=y$$ line, a linear transformation. But WTH are these factors of $$\frac{\sqrt{3}-1}{2}$$ and $$\frac{\sqrt{3}-3}{6}$$? I kinda worked it out, and the solution is as follows.
 
 Two facts about the skewing:
 
@@ -43,7 +43,7 @@ Two facts about the skewing:
 
 So, we need to get $$(x',y')$$ given $$(x,y)$$ and the quantity $$(x+y)$$. It's a linear transformation, so $$(x+y)$$ must be involved. Let's pick a point, add $$(x+y)$$ times some constant, and figure out what this constant has to be.
 
-Not rigorous? I did graduate with a physics major. XD
+(Not rigorous? I did graduate with a physics major. XD)
 
 So let's take that point that's just north-east of the origin. What are its coordinates? Turns out it's at $$(\frac{1}{\sqrt{3}},\frac{1}{\sqrt{3}})$$. This makes the triangles $$\sqrt{\frac{2}{3}}$$ long on a side. Why? Dunno. I guess Ken Perlin came up with the quantity and everyone kinda followed along. This length does not seem to be specified anywhere, and assuming a different length produces a different result.
 
@@ -53,4 +53,4 @@ $$\frac{1}{\sqrt{3}} + \frac{2}{\sqrt{3}}k = 1$$
 
 And lo! $$k = \frac{\sqrt{3}-1}{2}$$.
 
-Likewise, if we wanna unskew, we solve $$1 + (1+1)k' = \frac{1}{\sqrt{3}}$$ to get $$k' = \frac{\sqrt{3}-3}{6}$$.
+Likewise, if we wanna unskew from $$(1,1)$$ to $$(\frac{1}{\sqrt{3}},\frac{1}{\sqrt{3}})$$, we solve $$1 + (1+1)k' = \frac{1}{\sqrt{3}}$$ to get $$k' = \frac{\sqrt{3}-3}{6}$$.
---
+++ js/stage.js
@@ -0,0 +1,173 @@
+Stage = function() {
+  // webgl viewport
+  this.container = null;
+  this.camera = null;
+  this.scene = null;
+  this.renderer = null;
+
+  this.initViewport();
+}
+
+Stage.prototype.initViewport = function() {
+  var width, height;
+  var _this = this;
+
+  init();
+  animate();
+
+  function init() {
+    _this.container = document.getElementById('header');
+    height = _this.container.offsetHeight;
+    width = _this.container.offsetWidth;
+
+    _this.camera = new THREE.PerspectiveCamera(30, width/height, .1, 100000);
+
+    _this.scene = new THREE.Scene();
+    _this.scene.background = new THREE.Color(0x333333);
+
+    _this.controls = new Controls(
+      _this.camera,
+      _this.container,
+      {
+        r: 10,
+        phi: Math.PI/2,
+        theta: 3*Math.PI/5
+      }
+    );
+
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0x1);
+    _this.scene.add(ambientLight);
+
+    var sphereGeo = new THREE.SphereGeometry(20000, 32,15);
+    var sphereMat = new THREE.ShaderMaterial({
+      uniforms: {
+        sunPos: { value: new THREE.Vector3(0,6680,-18851) },
+        turbidity: { value: 10 },
+        mieCoefficient: { value: 1 },
+        g: { value: .8 },
+        luminance: { value: 1.1 }
+      },
+      vertexShader: sun_vert,
+      fragmentShader: sun_frag,
+      side: THREE.BackSide
+    });
+    var sphere = new THREE.Mesh(sphereGeo, sphereMat);
+    _this.scene.add(sphere);
+
+    _this.controls.update();
+
+    /* RENDER */
+    _this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    _this.renderer.shadowMap.enabled = true;
+    _this.renderer.toneMapping = THREE.ReinhardToneMapping;
+    _this.renderer.setPixelRatio(window.devicePixelRatio);
+    _this.renderer.setSize(width, height);
+    _this.container.appendChild(_this.renderer.domElement);
+
+    addEventListeners();
+  }
+
+  function addEventListeners() {
+    window.addEventListener('resize', onWindowResize, false);
+  }
+
+  function onWindowResize() {
+    height = _this.container.offsetHeight;
+    width = _this.container.offsetWidth;
+    _this.camera.aspect = width/height;
+    _this.camera.updateProjectionMatrix();
+
+    _this.renderer.setSize(width, height);
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+    render();
+  }
+
+  function render() {
+    if (!_this.camera || !_this.scene) return;
+    _this.controls.update();
+    _this.renderer.render(_this.scene, _this.camera);
+  }
+}
+
+
+var sun_vert = " \
+uniform vec3 sunPos; \
+uniform float turbidity; \
+uniform float mieCoefficient; \
+\
+varying vec3 vWorldPos; \
+varying vec3 vSunDir; \
+varying float vSunfade; \
+varying vec3 vBetaM; \
+varying float vSunE; \
+\
+const vec3 up = vec3(0.0, 1.0, 0.0); \
+\
+const float e = 2.71828182845904523536028747135266249775724709369995957; \
+const float pi = 3.141592653589793238462643383279502884197169; \
+\
+const float v = 4.0; \
+const vec3 K = vec3(0.686, 0.678, 0.666); \
+\
+const vec3 lambda = vec3(60E-9, 550E-9, 850E-9); \
+\
+void main() { \
+  vec4 worldPos = modelMatrix * vec4(position, 1.0); \
+  vWorldPos = worldPos.xyz; \
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0); \
+  \
+  vSunDir = normalize(sunPos); \
+  \
+  float sunTheta = acos(dot(vSunDir, up)); \
+  vSunE = 1000.0 * max(0.0, 1.0 - pow(e, -(sunTheta/49.5))); \
+  vSunfade = clamp(exp(sunPos.y / length(sunPos)), -1.0, 1.0); \
+  \
+  float c = (0.2 * turbidity) * 10E-18; \
+  vBetaM = (0.434 * c * pi * pow((2.0 * pi) / lambda, vec3(v-2.0)) * K) * mieCoefficient; \
+}";
+var sun_frag = " \
+\
+varying vec3 vWorldPos; \
+varying vec3 vSunDir; \
+varying float vSunfade; \
+varying vec3 vBetaM; \
+varying float vSunE; \
+\
+uniform float luminance; \
+uniform float g; \
+\
+const float pi = 3.141592653589793238462643383279502884197169; \
+\
+const float mieZenithLength = 1.25e3; \
+const vec3 up = vec3(0.0, 1.0, 0.0); \
+\
+const float A = 0.15; \
+const float B = 0.50; \
+const float C = 0.10; \
+const float D = 0.20; \
+const float E = 0.02; \
+const float F = 0.30; \
+\
+void main() { \
+  float thetaZ = acos(max(0.0, dot(up, normalize(vWorldPos)))); \
+  float sM = mieZenithLength / (cos(thetaZ) + 0.15 * pow(1.63860237 - thetaZ, -1.253)); \
+  \
+  vec3 Fex = exp(-vBetaM * sM); \
+  \
+  float cosTheta = dot(normalize(vWorldPos), vSunDir); \
+  \
+  float mPhase = (1.0 / (4.0*pi)) * (1.0-pow(g, 2.0)) / pow(1.0 - 2.0*g*cosTheta + pow(g, 2.0), 1.5); \
+  vec3 betaMTheta = vBetaM * mPhase; \
+  \
+  vec3 Lin = pow(vSunE * mPhase * (1.0 - Fex),vec3(1.5)); \
+  \
+  vec3 x = (log2(2.0/pow(luminance,4.0))) * Lin * 0.04; \
+  vec3 color = ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F; \
+  \
+  vec3 retColor = pow(color, vec3(1.0/(1.2+(1.2*vSunfade)))); \
+  gl_FragColor.rgb = retColor + 0.1; \
+  gl_FragColor.a = 1.0; \
+}";
