6b1790771bb5983ea3ce9630981da77bb1f89467
Anton Bovin <0x00019913@gmail.com>
2017-03-18 17:12:13 -0500

    temporary - add way too much noise

--- js/sceneLoader.js
+++ js/sceneLoader.js
@@ -322,7 +322,7 @@ void main() { \
   vec3 color1 = 0.9*vec3(0.5, 0.3, 0.2); \
   vec3 color = mix(color0, color1, level); \
   color *= color*2.0; \
-  color += 1.5*clamp(vPos.y/noiseScale.y * 10.0 - 5.3, 0.0, 1.0)*color0; \
+  color += 10.5*clamp(vPos.y/noiseScale.y * 10.0 - 5.3, 0.0, 1.0)*color0; \
   gl_FragColor = vec4(color, 0.0); \
 }";
 
